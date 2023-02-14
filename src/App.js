cdc8cc0898c9564b6c3162c525a2e76bd549ec16
Riku Viljanen <riku.viljanen@gmail.com>
2023-02-14 10:20:53 +0200

    changed site url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import react, {useState} from 'react';
+
 
 function App() {
+
+const [profile, setProfile] = useState({user: "", password: ""});
+
+const inputChanged = (e) => {
+  setProfile({...profile, [e.target.name]: e.target.value});
+}
+
   return (
     <div className="App" style={{color:'red'}}>
         <h1>My website headline</h1>
         <p>Heres some bs text too</p>
+        <form>
+        <div>  
+          <input placeholder='Username' name='user' value={profile.user} onChange={inputChanged}/>
+        </div>
+        <div>
+          <input placeholder='Password' name='password' value={profile.password} onChange={inputChanged}/>
+        </div>
+        <div>
+          <input value='Press teh button' type='submit'/>
+        </div>
+        </form>
     </div>
   );
 }
