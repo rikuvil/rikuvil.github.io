82afff45b8aaa5093aa14f2907ceae8fa8ab7046
Riku Viljanen <riku.viljanen@gmail.com>
2023-02-14 09:59:07 +0200

    finishing react setup

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{color:'red'}}>
+        <h1>My website headline</h1>
+        <p>Heres some bs text too</p>
     </div>
   );
 }
